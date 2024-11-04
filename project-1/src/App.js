f0a90aed4b962e0e588f31bbbebe2b8d4b3b5c35
64reth <garetha81@gmail.com>
2024-11-04 22:16:39 +0000

    made changes to customer message on line #10

--- project-1/src/App.js
+++ project-1/src/App.js
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="app-container">
       <h1 className="app-heading">Welcome to My Home Page!</h1>
-      <p className="app-message">
-        Thank you for visiting. We're glad you're here!
-      </p>
+      <p className="app-message">We thank you for visiting. Come back soon!</p>
     </div>
   );
 }
