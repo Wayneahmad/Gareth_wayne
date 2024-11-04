b5a8cfe06c1eac7dc20efcf9bd0a10c09848e065
64reth <Garetha81@gmail.com>
2024-11-04 22:06:44 +0000

    Setting up react project (#1)
    * uploaded react app
    
    * updated readme with further instructions
    
    * made changes further
    
    * made changes
    
    * Made additional changes meow
    
    ---------
    
    Co-authored-by: wayneahmad <waynesphotoz@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Gareth_wayne
+
 experimental project
+
+# make a react project basic website with 2 pages
+
+# potential features to implement will logging in a user
+
+# make sure to npm install when a new project is pulled
+
+# meow went the cat
---
+++ project-1/src/App.js
@@ -0,0 +1,17 @@
+// App.js
+
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-heading">Welcome to My Home Page!</h1>
+      <p className="app-message">
+        Thank you for visiting. We're glad you're here!
+      </p>
+    </div>
+  );
+}
+
+export default App;
